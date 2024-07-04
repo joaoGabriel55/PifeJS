3253cb30be5d11864f873f92426c1c4cc2899048
Daniel M Brasil <danielmbrasil@protonmail.com>
2024-07-04 17:38:26 -0300

    check winner

---
+++ calculate-winner.js
@@ -0,0 +1,49 @@
+function isValidSet(cards) {
+  const newCards = cards.map((card) => {
+    if (isNaN(card.value)) {
+      return card;
+    } else {
+      return { value: Number(card.value), suit: card.suit };
+    }
+  });
+
+  function isSequence(cards) {
+    const values = cards.map((card) => card.value).sort((a, b) => a - b);
+
+    return (
+      (values[0] + 1 === values[1] && values[1] + 1 === values[2]) ||
+      (values.includes("A") && values.includes(2) && values.includes(3)) || // A, 2, 3
+      (values.includes("Q") && values.includes("K") && values.includes("A")) // Q, K, A
+    );
+  }
+
+  function isSameValue(cards) {
+    return (
+      cards[0].value === cards[1].value &&
+      cards[1].value === cards[2].value &&
+      new Set(cards.map((card) => card.suit)).size === 3
+    );
+  }
+
+  function findSets(cards, setsFound) {
+    if (setsFound === 3) return true;
+    if (cards.length < 3) return false;
+
+    for (let i = 0; i < cards.length - 2; i++) {
+      for (let j = i + 1; j < cards.length - 1; j++) {
+        for (let k = j + 1; k < cards.length; k++) {
+          const selectedCards = [cards[i], cards[j], cards[k]];
+          if (isSameValue(selectedCards) || isSequence(selectedCards)) {
+            const remainingCards = cards.filter(
+              (_, index) => index !== i && index !== j && index !== k
+            );
+            if (findSets(remainingCards, setsFound + 1)) return true;
+          }
+        }
+      }
+    }
+    return false;
+  }
+
+  return findSets(newCards, 0);
+}
--- index.js
+++ index.js
@@ -5,6 +5,8 @@ function initGame() {
   const playerDiv = document.getElementById("player");
   const deckDiv = document.getElementById("deck-pile");
   const discardPileDiv = document.getElementById("discard-pile");
+  const discardedPileContentDiv = document.getElementById("discarded-pile");
+  const discardedCards = [];
 
   const shuffledDeck = shuffleDeck(DECK);
   const { deck, playerCards, opponentCards } = distributeCards(shuffledDeck);
@@ -21,8 +23,14 @@ function initGame() {
   discardPileDiv.addEventListener("drop", handleDrop);
   discardPileDiv.addEventListener("dragover", handleDragOver);
 
-  eventBus.addEventListener('test-event', ({ detail}) => {
+  eventBus.addEventListener("update-player-cards-event", () => {
     addDeckDragAndDropEvent();
+  });
+
+  eventBus.addEventListener("player-move-event", () => {
+    if (isValidSet(playerCards)) {
+      alert("you won");
+    }
   });
 
   let dragSrcEl = null;
@@ -57,10 +65,13 @@ function initGame() {
   function handleDrop(e) {
     if (e.stopPropagation) e.stopPropagation();
     if (!dragSrcEl) return;
-    if (!deck[deck.length - 1].isFaceUp) return;
+    if (!deck[deck.length - 1].isFaceUp && dragSrcEl.parentNode === deckDiv) return;
 
-    if (dragSrcEl.parentNode === deckDiv || dragSrcEl.parentNode === discardPileDiv) {
+    if (dragSrcEl.parentNode === deckDiv) {
       handleDeckCardDrop(dragSrcEl, this);
+    } else if (dragSrcEl.parentNode === discardedPileContentDiv) { // last card
+      console.log('comprar do descarte');
+      handleDiscardedCardDrop(dragSrcEl, this);
     } else if (dragSrcEl != this) {
       const content = e.dataTransfer.getData("text/html");
       handlePlayerCardSwap(dragSrcEl, this, content);
@@ -88,9 +99,14 @@ function initGame() {
       const drawnCard = deck.pop();
 
       const targetIndex = Number(targetElement.dataset.index);
-      const { cardContent, discardedCard } = drawCard({ targetIndex, cards: playerCards, drawnCard });
+      const { cardContent, discardedCard } = drawCard({
+        targetIndex,
+        cards: playerCards,
+        drawnCard,
+      });
 
       updatePlayerAndDiscardPiles(targetIndex, cardContent, discardedCard);
+      eventBus.dispatchEvent(new CustomEvent("player-move-event"));
     } else {
       discardCard();
     }
@@ -98,14 +114,28 @@ function initGame() {
     sourceElement.remove();
   }
 
-  function updatePlayerAndDiscardPiles(targetIndex, cardContent, discardedCard) {
+  function updatePlayerAndDiscardPiles(
+    targetIndex,
+    cardContent,
+    discardedCard
+  ) {
     const playerDiv = document.getElementById("player");
     const deckCards = document.querySelectorAll(".deck .content .card");
     const topCard = deckCards.item(deckCards.length - 1);
-    const discardedPileContent = document.querySelector(".discarded-cards .content");
+    const discardedPileContent = document.querySelector(
+      ".discarded-cards .content"
+    );
+
+    const lastDiscardedCard = createCard({ ...discardedCard, isFaceUp: true })
 
     playerDiv.children[targetIndex].innerHTML = cardContent;
-    discardedPileContent.appendChild(createCard({ ...discardedCard, isFaceUp: true }));
+    discardedPileContent.appendChild(
+      lastDiscardedCard
+    );
+
+    lastDiscardedCard.addEventListener("dragstart", handleDeckDragStart);
+    lastDiscardedCard.addEventListener("dragover", handleDragOver);
+    lastDiscardedCard.addEventListener("dragend", handleDragEnd);
 
     const cards = document.querySelectorAll(".discarded-cards .content .card");
 
@@ -115,7 +145,9 @@ function initGame() {
 
     topCard.remove();
 
-    eventBus.dispatchEvent(new CustomEvent('test-event', { detail: 'data' }));
+    setDiscardedCards(discardedCard);
+
+    eventBus.dispatchEvent(new CustomEvent("update-player-cards-event"));
   }
 
   function handlePlayerCardSwap(sourceElement, targetElement, content) {
@@ -139,25 +171,58 @@ function initGame() {
 
     topCard.addEventListener("click", () => {
       deck[deck.length - 1].isFaceUp = true;
-      flipCard(topCard, topCardData.suit, topCardData.value)
+      flipCard(topCard, topCardData.suit, topCardData.value);
     });
   }
 
   function discardCard() {
-    const discardedPileContent = document.querySelector(".discarded-cards .content");
+    const discardedPileContent = document.querySelector(
+      ".discarded-cards .content"
+    );
     const deckCards = document.querySelectorAll(".deck .content .card");
     const topCard = deckCards.item(deckCards.length - 1);
     const topCardData = deck.pop();
+    setDiscardedCards(topCardData);
 
-    discardedPileContent.appendChild(createCard({ ...topCardData, isFaceUp: true }));
+    const lastDiscardedCard = createCard({ ...topCardData, isFaceUp: true });
+
+    // TODO refactor (repeated in addDeckDragAndDropEvent)
+    lastDiscardedCard.addEventListener("dragstart", handleDeckDragStart);
+    lastDiscardedCard.addEventListener("dragover", handleDragOver);
+    lastDiscardedCard.addEventListener("dragend", handleDragEnd);
+
+    discardedPileContent.appendChild(
+      lastDiscardedCard
+    );
     topCard.remove();
 
-    eventBus.dispatchEvent(new CustomEvent('test-event', { detail: 'data' }));
+
+    eventBus.dispatchEvent(new CustomEvent("update-player-cards-event"));
 
     const cards = document.querySelectorAll(".discarded-cards .content .card");
 
     cards.forEach((card, index) => {
       card.style.transform = `translate(${index * -5}px, 0)`;
     });
+  }
+
+  function setDiscardedCards(card) {
+    discardedCards.push(card);
+    console.log(discardedCards);
+  }
+
+  function handleDiscardedCardDrop(srcElement, targetElement) {
+    if (targetElement.dataset.player === "player") {
+      const drawnCard = discardedCards.pop();
+
+      const targetIndex = Number(targetElement.dataset.index);
+      const { cardContent, discardedCard } = drawCard({
+        targetIndex,
+        cards: playerCards,
+        drawnCard,
+      });
+
+      updatePlayerAndDiscardPiles(targetIndex, cardContent, discardedCard);
+    }
   }
 }
